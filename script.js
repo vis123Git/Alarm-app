0c78797c53e184e2f23d6b5df046f74f2cdc9f52
VishalKumarSain <vishaldhoria007@gmail.com>
2023-09-30 21:46:29 +0530

    push

--- script.js
+++ script.js
@@ -30,17 +30,17 @@ setButton.addEventListener("click", () => {
 
 //IF YOU DONT HAVE ANY TIME THAN DISPLAY MESSAGE, NO TIMER
 let timers = [];
-if (timers.length == 0) {
-  const currenttimers = document.querySelector(".current-timers");
-  const noTimer = document.createElement("p");
-  noTimer.textContent = "You have no timers currently!";
-  currenttimers.appendChild(noTimer);
-}
+if (timers?.length == 0) {
+        const currenttimers = document.querySelector(".current-timers");
+        const noTimer = document.createElement("p");
+        noTimer.textContent = "You have no timers currently!";
+        if(currenttimers)currenttimers.appendChild(noTimer);
+      }
 
 function startNewTimer(hours, minutes, seconds) {
   //REMOVE NO CURRENT TIMER MESSAGE
   const currenttimers = document.querySelector(".current-timers>p");
-  currenttimers.remove();
+  if(currenttimers)currenttimers.remove();
   const totalSeconds = hours * 3600 + minutes * 60 + seconds;
   const timer = {
     hours,
@@ -96,43 +96,35 @@ function displayTimer(timer) {
   const currentTimersSection = document.querySelector(".current-timers");
   currentTimersSection.appendChild(timerElement);
 
-  // CREATE ANOTHER DIV FOR STOP BUTTON
-  const stopButtonDiv = document.createElement("div");
-  stopButtonDiv.classList.add("set-time-button");
-  const stopButton = document.createElement("button");
-  stopButton.textContent = "Stop";
-  stopButton.classList.add("stop");
-  stopButtonDiv.appendChild(stopButton);
-  timerElement.appendChild(stopButtonDiv);
-
-  let intervalId; 
-  let remainingTime = timer.totalSeconds;
-
-  stopButton.addEventListener("click", () => {
-    if (stopButton.textContent === "Stop") {
-      clearInterval(intervalId); 
-      stopButton.textContent = "Resume";
-    } else if (stopButton.textContent === "Resume") {
-      // Resume the timer by creating a new interval
-      intervalId = setInterval(() => {
-        remainingTime--;
-
-        updateTimerDisplay(timer);
-
-        if (remainingTime <= 0) {
-          clearInterval(intervalId);
-          handleTimerCompletion(timer);
+   // CREATE ANOTHER DIV FOR STOP/RESUME BUTTON
+   const stopButtonDiv = document.createElement("div");
+   stopButtonDiv.classList.add("set-time-button");
+   const stopButton = document.createElement("button");
+   stopButton.textContent = "Delete";
+   stopButton.classList.add("stop");
+   stopButtonDiv.appendChild(stopButton);
+   timerElement.appendChild(stopButtonDiv);
+ 
+   stopButton.addEventListener("click", () => {
+        if (stopButton.textContent == "Delete") {
+          const timerTab = document.querySelector(".timer-tab2");
+          timerTab.remove();
+          delete timer;
+          if (timers?.length == 0) {
+                const currenttimers = document.querySelector(".current-timers");
+                const noTimer = document.createElement("p");
+                noTimer.textContent = "You have no timers currently!";
+                if(currenttimers)currenttimers.appendChild(noTimer);
+              }
+          return;
         }
-      }, 1000);
-      
-      stopButton.textContent = "Stop"; 
-    }
-  });
+      });
 }
 
 function updateTimerDisplay(timer) {
   const timerElement = document.querySelector(`[data-timer-id="${timer.id}"]`);
-  const timerTimeElement = timerElement.querySelector(".timer-time");
+  let  timerTimeElement;
+  if(timerElement)  timerTimeElement = timerElement.querySelector(".timer-time");
 
   const hours = Math.floor(timer.totalSeconds / 3600);
   const minutes = Math.floor((timer.totalSeconds % 3600) / 60);
@@ -142,26 +134,28 @@ function updateTimerDisplay(timer) {
   const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
   // Update the timer display
-  timerTimeElement.textContent = formattedTime;
+  if(timerTimeElement)timerTimeElement.textContent = formattedTime;
 }
 
 function handleTimerCompletion(timer) {
-  const timerElement = document.querySelector(`[data-timer-id="${timer.id}"]`);
-
-  if (timerElement) {
-    timerElement.classList.add("completed-timer");
-
-    const stopButton = timerElement.querySelector(".stop");
-    if (stopButton) {
-      stopButton.textContent = "Delete";
-    }
-
-    stopButton.addEventListener("click", () => {
-      if (stopButton.textContent == "Delete") {
-        const timerTab = document.querySelector(".timer-tab2");
-        timerTab.remove();
-        return;
+        const timerElement = document.querySelector(`[data-timer-id="${timer.id}"]`);
+      
+        if (timerElement) {
+          timerElement.classList.add("completed-timer");
+      
+          const stopButton = timerElement.querySelector(".stop");
+          if (stopButton) {
+            stopButton.textContent = "Delete";
+      
+            stopButton.removeEventListener("click", null);
+          }
+      
+          stopButton.addEventListener("click", () => {
+            if (stopButton.textContent == "Delete") {
+              const timerTab = document.querySelector(".timer-tab2");
+              timerTab.remove();
+              return;
+            }
+          });
+        }
       }
-    });
-  }
-}
